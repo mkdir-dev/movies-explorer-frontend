6e39b85de9fe216456376f89b0c916395d8d7b5e
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-08-27 18:52:51 +0500

    feat: create a section Techs for the main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,12 +4,14 @@ import './Main.css';
 
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
     <>
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   );
 }
